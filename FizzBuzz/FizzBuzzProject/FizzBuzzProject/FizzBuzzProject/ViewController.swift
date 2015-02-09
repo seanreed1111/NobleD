cde80c04d28123a5d5dd2c1197acdb488b2d7df0
Sean Reed <seanreed1111@gmail.com>
2015-02-09 13:50:38 -0500

    finishes FizzBuzzProject as an Xcode6.1.1 project, so students get used to working outside of playgrounds

--- FizzBuzz/FizzBuzzProject/FizzBuzzProject/FizzBuzzProject/ViewController.swift
+++ FizzBuzz/FizzBuzzProject/FizzBuzzProject/FizzBuzzProject/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var fbObject = FizzBuzz()
+    @IBOutlet weak var textField: UITextField!
 
+    @IBOutlet weak var fizzBuzzLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // textField delegate methods
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool{
+        // implement textfield validation here
+        // called BEFORE the text field resigns first responder status
+        if let number = textField.text.toInt() {
+            fizzBuzzLabel.text = "FizzBuzz of \"\(number)\" is \(fbObject.fizzBuzz(number))"
+            return true
+        }
+        textField.text = nil
+        return false
     }
 
+    func textFieldShouldReturn (textField: UITextField) -> Bool{
+        // true if the text field should implement its default behavior for the return button; otherwise, false.
+        textField.resignFirstResponder()
+        return true
+    }
 
+    // exercise: How do we make this so that if the use taps
+    //anywhere on the keyboard, it will also come out of the text field
 }
+
+
 
