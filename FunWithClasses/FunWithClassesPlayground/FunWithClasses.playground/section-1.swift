5aa979c0a8cb1a94486f8045719709bb77861fff
Sean Reed <seanreed1111@gmail.com>
2015-02-12 11:56:42 -0500

    adds exercises to FunWithClasses and FunWithArrays

--- FunWithClasses/FunWithClassesPlayground/FunWithClasses.playground/section-1.swift
+++ FunWithClasses/FunWithClassesPlayground/FunWithClasses.playground/section-1.swift
@@ -11,6 +11,52 @@ import UIKit
 
 
 
+//Exercises
+//
+//
+
+// Using an array of Strings, create a new class Stack that implements a stack data structure. (need mutating functions)
+// Stacks are a ubitquitous last-in, first-out (LIFO) structure
+
+// One way to visualize a stack is to think about a small clown car with one door.
+
+// Our Stack will have three methods
+//1) push(item: String) that will add a new String to the stack
+//2) pop()-> String that will remove the last item pushed to the stack, and return that item
+// 3) peek()-> String that will print the last item pushed to the stack but does NOT remove the item.
+
+// push("Clown1")
+// push("Clown2")
+// push("Clown3")
+// push("Clown4")
+//
+// after we do these four actions, our stack is now [Clown1, Clown2, Clown3, Clown4]
+
+// pop()  // returns "Clown4". Stack is now [Clown1, Clown2, Clown3]
+
+// pop()  //returns "Clown3". Stack is now [Clown1, Clown2]
+// peek() //returns "Clown2". Stack remains [Clown1, Clown2]
+// push("Clown5") // Stack is now [Clown1, Clown2, Clown5]
+
+// and so forth
+
+///
+//
+//
+
+// Part 1 - Using Enums, build a class called PlayingCard
+
+// PlayingCard should have properties that store its suit and rank separately. 
+// It should also have an init method to form a new card given the rank and suit as input parameters.
+// Finally, it should have a method (or computed property) that returns its rank and suit together as a String, for example "A of Hearts", or "3 of Diamonds"
+
+// Part 2 - Using an Array of your newly created PlayingCard class, build a class called Deck
+
+// Deck should have an init method that constructs a standard deck of 52 card.
+// Deck should have a method called dealFromTop that removes the first PlayingCard from the deck and prints its rank and suit as a String (need mutating function)
+// Deck should have a method called dealAll that deals all the cards remaining in the deck in a similar manner to dealFromTop
+
+
 //Apple Things
 //
 //
---
+++ TypesInSwift/CollectionTypes-ArraysAndDictionaries/FunWithArrays/FunWithArrays-Exercises.playground/section-1.swift
@@ -0,0 +1,48 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, Arrays"
+
+// Exercises
+
+// 1) write a program that starts with an array of integers and makes a corresponding array of Bools, true if the integer is even, false if the integer is odd
+
+// example 
+// if you get an inputArray of
+//inputArray [3,6,8,9]
+
+//then the outputArray is
+//outputArray: [Bool] = [false, true, true, false]
+
+
+// this operation is known as a map.
+// please do not use the map function of Swift to do this problem!!
+
+//2) write a program that starts with an array of integers and makes a new array with all integers > 50 removed 
+
+// example
+// if you get an inputArray of
+//inputArray = [30,60,80,90]
+
+//then the outputArray is
+//outputArray = [30]
+
+// this operation is known as a filter.
+// please do not use the filter function of Swift to do this problem!!
+
+//3) write a program that starts with an array of integers 
+//and computes the product of all the elements in the array
+
+// example
+// if you get an inputArray of
+//inputArray = [1,2,3,4,5]
+
+//then the output is
+//output = 120
+
+// this operation is known as reduce or fold.
+// please do not use the reduce function of Swift to do this problem!!
+
+
+
