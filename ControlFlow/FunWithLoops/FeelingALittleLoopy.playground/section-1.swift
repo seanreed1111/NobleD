50a13a21fa4348ac1885bd11345cc9b197616564
Sean Reed <seanreed1111@gmail.com>
2015-02-18 14:57:43 -0500

    adds switch-case playground and markdown file

---
+++ ControlFlow/FunWithLoops/FeelingALittleLoopy.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, loops"
+
+
+// For In loop - iterating over ranges
+// While loop
+// Iterating over arrays
+// Iterating over dictionaries
+
