ab29eddb56336c9fdbaa60df5f9f1e7964d0a5ba
Sean Reed <seanreed1111@gmail.com>
2015-02-23 18:33:35 -0500

    adds Hello World With Gravity and collision and elasticity

---
+++ UIKitDynamics/HelloWorldWithUIKitDynamics/HelloWorldWithUIKitDynamics/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  HelloWorldWithUIKitDynamics
+//
+//  Created by Sean Reed on 2/21/15.
+//  Copyright (c) 2015 Sean Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIDynamicAnimatorDelegate {
+    @IBOutlet weak var label: UILabel!
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    var bounce: UIDynamicItemBehavior!
+    
+    override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        
+        bounce  = UIDynamicItemBehavior(items: [label])
+        bounce.elasticity = CGFloat(0.76)
+        
+        gravity = UIGravityBehavior(items: [label])
+        gravity.gravityDirection = CGVectorMake(0,1)
+        
+        
+        collision = UICollisionBehavior(items: [label])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        
+        animator.addBehavior(bounce)
+        animator.addBehavior(collision)
+        animator.addBehavior(gravity)
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
